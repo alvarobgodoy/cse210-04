35a43d96f358c679e79bd995ecb08a5e4ab349d8
Ismael <rojasisma10@gmail.com>
2022-02-22 18:45:29 -0300

    Here are the classes
    The Location and Director Class

---
+++ greed/game/common/location.py
@@ -0,0 +1,81 @@
+
+
+
+class Location:
+    '''This class is the represents the distance of the origin (0, 0)
+    
+    The responsibility of Location is to save and provide 
+    information about the position of the element that holds this class. 
+    
+    Attributes:
+        _x (interger): The horizontal distance of the origin
+        _y (interger): The vertical distance of the origin
+    '''
+    def __init__(self, x, y):
+        ''' Constructs the new location with the given x and y values
+
+        Args:
+            x (int): the given x value
+            y (int): the given y value
+        '''
+        self._x = x
+        self._y = y
+
+    def add(self, other):
+        '''Gets a new location that is the sum of this 
+        and the given one
+        
+        Args:
+            other (Location): The location to add.
+        
+        Returns:
+            Location: A new location that is the sum.
+            
+        '''
+        x = self._x + other.get_x()
+        y = self._y + other.get_y()
+        return Location(x, y)
+
+
+
+    def equals(self, other):
+        '''Whether or not yhid Location is equal to the given one.
+        
+        Args: 
+            other (Location): the Location to compare
+
+        Returns:
+            boolean: True if both x and y are equal; false if otherwise.
+
+        '''
+        return self._x == other.get_x() and self._y == other.get_y()
+
+    def get_x(self):
+        '''Gets the horizontal distance.
+        
+        Returns:
+            interger: The horizontal distance
+        '''
+        return self._x
+
+    def get_y(self):
+        '''Gets the vertical distance.
+        
+        Returns:
+            interger: The vertical distance'''
+        return self._y
+
+    def scale(self, factor):
+        '''Scales the location by the provided factor.
+        
+        Args:
+            factor (int): The amount to scale.
+            
+        Returns: 
+            Location: A new Location that is called'''
+        return Location(self._x * factor, self._y * factor)
+ 
+
+
+
+    
---
+++ greed/game/director/director.py
@@ -0,0 +1,83 @@
+# well... this is the class named director
+
+from game.services.waterfall import Waterfall
+
+class Director:
+    '''The one who directs the game 
+    
+    It gives the directions about what, which and when something has to act in the right moment
+
+    Attributes:
+        _keyboard_service
+        _video_service
+    
+    '''
+    def __init__(self, keyboard_service, video_service):
+        '''Constructs a new Director using the specified movement and graphic services
+        
+        Args:
+            keyboard_service
+        '''
+        self._points = 0
+        self._waterfall = Waterfall()
+
+        self._video_service = video_service
+        self._keyboard_service = keyboard_service
+
+
+    def start_game(self, participants):
+        ''''''
+        self._video_service.open_window()
+        while self._video_service.is_window_open():
+            self.get_inputs(participants)                    
+            self.do_updates(participants)                
+            self.do_outputs(participants)
+        self._video_service.close_window()
+
+
+    def get_inputs(self, participants):
+        ''''''
+        excavator = participants.get_first_participant('excavator')
+        velocity = self._keyboard_service.get_direction()
+        excavator.set_velocity(velocity)
+
+
+    def do_updates(self, participants):
+        ''''''        
+        banner = participants.get_participants('banner')
+        excavator = participants.get_first_participant('excavator')
+        minerals = participants.get_participants('minerals')
+
+        banner.set_text('')
+        max_x = self._video_service.get_width()
+        max_y = self._video_service.get_height()
+        excavator.move_next(self._max_x)
+
+
+        self._waterfall.generate_line()
+        self._waterfall.move_down(participants, max_x, max_y)
+        self._waterfall.remove_last_line(participants)
+
+        for mineral in minerals:
+            if excavator.get_position().equals(mineral.get_position()):
+                mineral_points = mineral.get_points()
+                self._points += mineral_points
+                participants.remove_actor(mineral, )
+        banner.set_text(f'Score: {self._points}')
+            
+
+
+
+    def do_outputs(self, participants):
+        ''''''     
+
+        self._video_service.clear_buffer()
+        entities = participants.get_all_minerals()
+        self._video_service(entities)
+        self._video_service.flush_buffer()
+
+
+
+
+
+
