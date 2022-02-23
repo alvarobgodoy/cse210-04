b041ba3ce2520c698b1948e7d161637ada83ba22
Ismael <rojasisma10@gmail.com>
2022-02-23 18:55:29 -0300

    just added some comments to clear and stuff

--- greed/game/director/director.py
+++ greed/game/director/director.py
@@ -16,7 +16,8 @@ class Director:
         '''Constructs a new Director using the specified movement and graphic services
         
         Args:
-            keyboard_service
+            keyboard_service (KeyboardService): An instance of KeyboardService
+            video_service (VideoService): An instance of VideoService           
         '''
         self._points = 0
         self._waterfall = Waterfall()
@@ -26,7 +27,12 @@ class Director:
 
 
     def start_game(self, participants):
-        ''''''
+        '''Starts the the game using the given participants. Runs 
+        the main game loop
+        
+        Args:
+            participants (Participants): the group of all the participants
+        '''
         self._video_service.open_window()
         while self._video_service.is_window_open():
             self.get_inputs(participants)                    
@@ -36,14 +42,22 @@ class Director:
 
 
     def get_inputs(self, participants):
-        ''''''
+        '''Gets the directions from the inputs of the user 
+        from the keyboard
+        
+        Args:
+            participants (Participants): the group of all the participants'''
         excavator = participants.get_first_participant('excavator')
         velocity = self._keyboard_service.get_direction()
         excavator.set_velocity(velocity)
 
 
     def do_updates(self, participants):
-        ''''''        
+        '''Updates the participant positions and resolves any collitions 
+        between the excavator and the minerals
+        
+        Args:
+            participants (Participants): the group of all the participants'''        
         banner = participants.get_participants('banner')
         excavator = participants.get_first_participant('excavator')
         minerals = participants.get_participants('minerals')
@@ -62,14 +76,17 @@ class Director:
             if excavator.get_position().equals(mineral.get_position()):
                 mineral_points = mineral.get_points()
                 self._points += mineral_points
-                participants.remove_actor(mineral, )
+                participants.remove_actor(mineral, 'minerals')
         banner.set_text(f'Score: {self._points}')
             
 
 
 
     def do_outputs(self, participants):
-        ''''''     
+        '''Draws the participants on the screen 
+        
+        Args:
+            participants (Participants): the group of all the participants'''     
 
         self._video_service.clear_buffer()
         entities = participants.get_all_minerals()
